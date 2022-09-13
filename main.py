70e08084e7db68abe275c4ae337a51f44d711b13
romanvetrin@gmail.com <romanvetrin@gmail.com>
2022-09-13 17:05:50 +0300

    task_4

--- main.py
+++ main.py
@@ -88,3 +88,41 @@ if __name__=='__main__':
     funh("test")
 
     decorator_3.display_ranks()
+
+
+#====================================================================
+
+from task_4 import decorator_4, decorator_4_
+
+
+@decorator_4
+def func():
+    print("I am ready to Start")
+    result = 0
+    n =  random.randint(10,751)
+    for i in range(n):
+        result += (i**2)
+
+@decorator_4
+def funx(n=2, m=5):
+    print("I am ready to do serious stuff")
+    max_val = float('-inf')
+    n =  random.randint(10,751)
+    res = [pow(i,2) for i in range(n)]
+    for i in res:
+        if i > max_val:
+            max_val = i
+
+@decorator_4
+def funh(x1, x2=5):
+    return x1/x2
+
+@decorator_4_
+def funh_1(x1, x2=5):
+    return x1/x2
+
+if __name__=='__main__':
+    funh(3, 6)
+    funh(5, 0)
+    funh_1(3, 6)
+    funh_1(5, 0)
---
+++ task_4.py
@@ -0,0 +1,96 @@
+import inspect
+from datetime import datetime
+from io import StringIO
+import sys
+from tabulate import tabulate
+
+class decorator_4:
+    ranks = {}
+    count = 0
+
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        time = datetime.now()
+
+        # changing stdout stream
+        out = StringIO()
+        old_stdout = sys.stdout
+        sys.stdout = out
+        try:
+            res = self.func(*args, **kwargs)
+        except Exception as e:
+            sys.stdout = old_stdout
+            with open('./error_log_task_4.txt', 'a') as f:
+                f.writelines(f'{datetime.now()} {e}\n')
+            print(f"{self.func.__name__} encountered error, see the logs")
+            return None
+
+        f_out = out.getvalue()
+
+        # count and time
+        self.count += 1
+        time = datetime.now() - time
+        print(f'{self.func.__name__} call {self.count} executed in {time.total_seconds()} sec')
+
+        # inspection
+        print(f'Name:\t{self.func.__name__}')
+        print(f'Type:\t{type(self.func)}')
+        sig = inspect.signature(self.func)
+        print(f'Sign:\t{sig}')
+        var = locals()
+        print(f'Args:\tpositional {var["args"]}\n\tkey=worded {var["kwargs"]}')
+        print(f'Doc:\t{self.func.__doc__}')
+        code = inspect.getsourcelines(self.func)
+        fl = True
+        for n in code[0]:
+            if fl:
+                fl = False
+                print(f'Source:\t{n[:-1]}')
+            else:
+                print(f'\t{n[:-1]}')
+        print(f'Output:\t{f_out}')
+
+        # recording info for ranks. currently only the best time is recorded
+        if not (self.func.__name__ in decorator_4.ranks) or \
+                decorator_4.ranks[self.func.__name__] > time.total_seconds():
+            decorator_4.ranks.update({self.func.__name__: time.total_seconds()})
+
+        sys.stdout = old_stdout
+        with open('./out_task_4.txt', 'a') as f:
+            print(out.getvalue(), file=f)
+
+        return res
+
+    @staticmethod
+    def get_zip_ranks():
+        return zip(list(decorator_4.ranks), decorator_4.ranks.values())
+
+    @staticmethod
+    def display_ranks():
+        decorator_4.ranks = {k: v for k, v in sorted(decorator_4.get_zip_ranks(), key=lambda item: item[1])}
+        name = decorator_4.ranks.keys()
+        rank = [i for i in range(1,len(decorator_4.ranks)+1)]
+        time = decorator_4.ranks.values()
+        print(tabulate(zip(name, rank, time), headers=['PROGRAM', 'RANK', 'TIME ELAPSED']))
+
+def decorator_4_(func):
+    count = 0
+    def wrapper(*args, **kargs):
+        nonlocal count
+        count += 1
+        time = datetime.now()
+
+        try:
+            out = func(*args, **kargs)
+        except Exception as e:
+            with open('./error_log_task_4.txt', 'a') as f:
+                f.writelines(f'{datetime.now()} {e}\n')
+            print(f"{func.__name__} encountered error, see the logs")
+            return None
+
+        time = datetime.now() - time
+        print(f'{func.__name__} call {count} executed in {time.total_seconds()} sec')
+        return out
+    return wrapper
